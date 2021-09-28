d969e5ef324f7464d5424295d3ce76aca1e0aecb
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-09-28 14:13:47 +0300

    Otus1_string

--- Otus1/otus1.js
+++ Otus1/otus1.js
@@ -3,3 +3,17 @@
 // prompt("Сколько вам лет");
 // confirm("все хорошо?");
 
+// //1.3.1 строки
+// let firstName = "Pavel";
+// let lastName = "Kryvaltsevich";
+// alert("Hello, " +  firstName + (" ") + lastName);
+
+// let firstName = "Pavel";
+// let lastName = "Kryvaltsevich";
+// alert(`Hello, ${firstName} ${lastName}`);
+
+// let name = prompt("введите ваше имя");
+// alert(`Привет, ${name}`);
+
+// let name = "Pavel";
+// alert(name.length);
