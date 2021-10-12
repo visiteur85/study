4fb0ce20c08cc94ac1d806ed4412b96591cf269a
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-10-12 14:35:32 +0300

    CWSimple multiplication

---
+++ Codewars/js.js
@@ -0,0 +1,14 @@
+//this kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+function simpleMultiplication(number) {
+    // your code........
+   if (number % 2 == 0) {
+        return number = number * 8;
+        
+    }
+
+    else {
+       return number = number * 9;
+       
+    }
+    
+};
