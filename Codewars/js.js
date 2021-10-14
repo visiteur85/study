a5d0add8464d716aa09103e0e6bcf48a25cedc89
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-10-14 22:36:30 +0300

    codewars_How old will I be in 2099?

--- Codewars/js.js
+++ Codewars/js.js
@@ -133,3 +133,18 @@ function greet() {
         { return true}
         else {return false};}
       
+        //Multiplication table for number
+        function multiTable(n) {
+            return [...Array(10)].map((_,i)=>`${i+1} * ${n} = ${n*i+n}`).join('\n')
+          }
+          //How old will I be in 2099?
+          function  calculateAge(a,b) {
+            if (a>b&&+a-b==1)return `You will be born in ${+a-b} year.`
+            else if (a<b&&+b-a==1)return `You are ${b-a} year old.`
+            else if (a>b)return `You will be born in ${+a-b} years.`
+            else if (a<b) return `You are ${b-a} years old.`
+            return 'You were born this very year!'
+          }
+
+
+          
