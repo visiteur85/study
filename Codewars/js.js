55da3e58bc13a4f9a6e4752e7ac2149fbf158ac9
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-10-15 09:00:48 +0300

    codewars_Watermelon function divide

--- Codewars/js.js
+++ Codewars/js.js
@@ -150,3 +150,10 @@ function greet() {
 function square(num) {
   return num * num;
 }
+
+//Watermelon function divide(weight){
+ let a=weight-2
+ let b=weight%2
+ if (a>1&&b==0)return true
+ return false
+}
