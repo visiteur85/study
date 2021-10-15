de12bb1876b3f4ec734cf13de1cd346279a57102
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-10-15 13:12:25 +0300

    freecode_Return Early Pattern for Functions

--- freeCodeCamp/freecode.js
+++ freeCodeCamp/freecode.js
@@ -309,3 +309,42 @@ function golfScore(par, strokes) {
     return names[6]
   }
 }
+                    //Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  
+  switch(val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+
+                    //Return Early Pattern for Functions
+                    // Setup
+function abTest(a, b) {
+  // Only change code below this line
+  
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+  
+  
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+console.log(abTest(-2,2));
