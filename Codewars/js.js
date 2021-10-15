ee9694732c77700412905cd450962107c28950f1
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-10-15 10:17:00 +0300

    codewars_Safen User Input Part I

--- Codewars/js.js
+++ Codewars/js.js
@@ -163,3 +163,11 @@ function square(num) {
 function take(arr, n) {
   return arr.slice(0, n);
 }
+
+//Safen User Input Part I - htmlspecialchars
+function htmlspecialchars(formData) {
+  return formData.replace(/&/g, "&amp;")
+                 .replace(/"/g, "&quot;")
+                 .replace(/</g, "&lt;")
+                 .replace(/>/g, "&gt;");
+}
