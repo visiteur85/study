aa176e12b6071bce991886502d6f44e705213b61
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-11-15 21:03:15 +0300

    codewars_Basic operations

--- Codewars/js.js
+++ Codewars/js.js
@@ -210,3 +210,12 @@ function boolToWord( bool ){
   }
   else {return "No"}
 }
+
+//Basic Mathematical Operations
+function basicOp(operation, value1, value2)
+{
+  if (operation == "+") {return value1 + value2 }
+  else if (operation == "-") {return value1 - value2 }
+  else if (operation == "*") {return value1 * value2 }
+  else { return value1 / value2}
+}
