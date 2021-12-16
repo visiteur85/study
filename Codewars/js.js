c42e178ddd7d7059180375d238faf32a6edc9f3d
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-12-16 20:07:42 +0300

    Codewars_kata_Transportation on vacation

--- Codewars/js.js
+++ Codewars/js.js
@@ -275,3 +275,12 @@ function typeOfSum(a, b) {
     return typeof(a + b);
 
 }
+
+//Transportation on vacation
+function rentalCarCost(d) {
+
+    let day = d >= 3 && d < 7 ? 20 : 0, // day discount
+        week = d >= 7 ? 50 : 0; // week discount
+
+    return (d * 40) - day - week;
+}
