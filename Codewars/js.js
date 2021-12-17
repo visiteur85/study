dada3d94999a631fac553a19ad653a0cb2c0edbb
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-12-17 23:04:05 +0300

    Codewars_kata_Century From Year

--- Codewars/js.js
+++ Codewars/js.js
@@ -345,3 +345,9 @@ var stringToNumber = function(str){
 function numberToString(num) {
     return num.toString()
 }
+//Century From Year
+function century(year) {
+    const result = String(year / 100).split('.')
+    return result.length === 2 ? Number(result[0]) + 1 : Number(result[0])
+        ;
+}
