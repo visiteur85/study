97425d8a225679a930c632d0a3b4adaad1cb9529
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-12-19 12:28:43 +0300

    Codewars_kata_Grasshopper - Messi Goals

--- Codewars/js.js
+++ Codewars/js.js
@@ -407,3 +407,10 @@ function reverseList(list) {
         {return true}
         else {return false}
     }
+
+    //Grasshopper - Messi Goals
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
