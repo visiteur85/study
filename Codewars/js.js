ab3222ca75d6eb57b22e98e2b50a1f079e758bd0
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-02-05 00:21:29 +0300

    Codewars_kata_Count of positives / sum of negatives

--- Codewars/js.js
+++ Codewars/js.js
@@ -467,3 +467,24 @@ const quarterOf = (month) => {
 function multiply (a, b)  {
     return a * b
 }
+
+//Count of positives / sum of negatives
+function countPositivesSumNegatives(input) {
+    let sumOfPositive = 0;
+    let sumOfNegative = 0;
+
+    if(input == null || input.length < 1) {
+        return [];
+    } else {
+        input.map(item => {
+            if(item > 0) {
+                sumOfPositive++;
+            } else if(item < 0) {
+                sumOfNegative += item;
+            } else {
+                return []
+            }
+        })
+    }
+    return [sumOfPositive, sumOfNegative]
+}
